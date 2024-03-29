504acaae9a1adcf3ab03a5ffab039c8ebf9307bc
raduSa <147979590+raduSa@users.noreply.github.com>
2024-03-29 21:55:59 +0200

    final

--- main.cpp
+++ main.cpp
@@ -7,51 +7,51 @@
 #include <thread>
 #include <stdexcept>
 #include <fstream>
+#include <algorithm>
 #define f cin
 #define g cout
+long long cnt;
 using namespace std;
-vector<int>get_number(int val_min,int val_max,int cnt)
+vector<long long>get_number(long long val_min,long long val_max,long long cnt)
 {
-    vector<int>a;
+    vector<long long>a;
     srand(static_cast<unsigned int>(std::time(nullptr)));
-    for(int i=0; i<cnt; i++)
+    for(long long i=0; i<cnt; i++)
     {
-        int randomNumber = val_min + rand() % (val_max - val_min + 1);
+        long long randomNumber = val_min + rand() % (val_max - val_min + 1);
         a.push_back(randomNumber);
     }
     return a;
 }
-
-vector<int>test_insertion(int val_min,int val_max,int cnt)
+vector<long long>test_insertion(long long val_min,long long val_max,long long cnt)
 {
 
-    vector<int>a;
+    vector<long long>a;
     const int n=10;
     srand(static_cast<unsigned int>(std::time(nullptr)));
-    for(int i=0; i<n; i++)
+    for(long long i=0; i<n; i++)
     {
-        int randomNumber = val_min + rand() % (val_max - val_min + 1);
+        long long randomNumber = val_min + rand() % (val_max - val_min + 1);
         a.push_back(randomNumber);
     }
-    for(int i=n;i<=cnt;i++)
+    for(long long i=n;i<=cnt;i++)
         a.push_back(i);
-    return a;
+    return a;
 }
-
-vector<int>a,cpy;
+vector<long long>a,cpy;
 bool inTime = true;
 struct TimeOutException : public std::exception {};
-void MergeSort(int st,int dr)
+void MergeSort(long long st,long long dr)
 {
     if (!inTime)
         throw TimeOutException{};
     if(st==dr)
         return;
-    int mj=st+(dr-st)/2;
+    long long mj=st+(dr-st)/2;
     MergeSort(st,mj);
     MergeSort(mj+1,dr);
-    int nr_st=st,nr_dr=mj+1,cnt=0;
-    int* b = new int[dr-st+1];
+    long long nr_st=st,nr_dr=mj+1,cnt=0;
+    long long* b = new long long[dr-st+1];
     while(nr_st<=mj&&nr_dr<=dr)
     {
         if(a[nr_st]<=a[nr_dr])
@@ -69,10 +69,10 @@ void MergeSort(int st,int dr)
 }
 void InsertionSort()
 {
-    for(int i=1; i<a.size(); i++)
+    for(long long i=1; i<a.size(); i++)
     {
-        int val=a[i];
-        int last=i-1;
+        long long val=a[i];
+        long long last=i-1;
         while(a[last]>val&&last>=0){
             if (!inTime)
                 throw TimeOutException{};
@@ -81,24 +81,24 @@ void InsertionSort()
         a[last+1]=val;
     }
 }
-void Insert_Heap(int val, int* h, int &nr)
+void Insert_Heap(long long val, long long* h, long long &nr)
 {
     h[++nr]=val;
-    int cnt=nr;
+    long long cnt=nr;
     while(h[cnt]<h[cnt/2]&&cnt>1)
     {
         swap(h[cnt],h[cnt/2]);
         cnt/=2;
     }
 }
-void Erase_Heap(int* h, int &nr)
+void Erase_Heap(long long* h, long long &nr)
 {
     swap(h[1],h[nr]);
     nr--;
-    int nod=1;
+    long long nod=1;
     while(true)
     {
-        int st=nod*2,dr=nod*2+1,mn=-1;
+        long long st=nod*2,dr=nod*2+1,mn=-1;
         if(st<=nr)
             mn=st;
         else
@@ -118,20 +118,18 @@ void Erase_Heap(int* h, int &nr)
 }
 void HeapSort()
 {
-    int* h = new int[a.size()];
-    int nr = 0;
+    long long* h = new long long[a.size()];
+    long long nr = 0;
     while(!a.empty())
     {
         if (!inTime)
             throw TimeOutException{};
-        int val=a.back();
+        long long val=a.back();
         a.pop_back();
         Insert_Heap(val, h, nr);
     }
-    // g<<'\n';
     while(nr>0)
     {
-        //g<<h[1]<<" ";
         if (!inTime)
             throw TimeOutException{};
         a.push_back(h[1]);
@@ -140,79 +138,76 @@ void HeapSort()
 }
 void RadixSort_10()
 {
-    int p=1,mx,n=a.size();
-    vector<int>nr(11),aux(n+1);
+    long long p=1,mx,n=a.size();
+    vector<long long>nr(11),aux(n+1);
     for(int i=0; i<n; i++)
         mx=max(mx,a[i]);
     for(int poz=1; p<=mx; poz++)
     {
         for(int i=0; i<10; i++)
             nr[i]=0;
-        for(int i=0; i<n; i++)
+        for(long long i=0; i<n; i++)
             nr[(a[i]/p)%10]++;
         for(int i=1; i<=9; i++)
             nr[i]+=nr[i-1];
-        for(int i=n-1; i>=0; i--)
+        for(long long i=n-1; i>=0; i--)
         {
             if (!inTime)
                 throw TimeOutException{};
             aux[nr[a[i]/p%10]]=a[i];
             nr[a[i]/p%10]--;
         }
-        for(int i=0; i<n; i++)
+        for(long long i=0; i<n; i++)
             a[i]=aux[i+1];
         p=p*10;
     }
-    std::vector<int>().swap(nr);
-    std::vector<int>().swap(aux);
+    std::vector<long long>().swap(nr);
+    std::vector<long long>().swap(aux);
 }
 void RadixSort_16()
 {
-    int p=1,mx,n=a.size();
+    long long p=1,mx,n=a.size();
     int n_16=(1<<16);
 
-    vector<int>nr(n_16),aux(a.size());
+    vector<long long>nr(n_16),aux(a.size());
     for(int poz=1; poz<=2; poz++)
     {
         for(int i=0; i<n_16; i++)
             nr[i]=0;
-        for(int i=0; i<n; i++)
+        for(long long i=0; i<n; i++)
             nr[(a[i]/p)%n_16]++;
         for(int i=1; i<n_16; i++)
             nr[i]+=nr[i-1];
-        for(int i=n-1; i>=0; i--)
+        for(long long i=n-1; i>=0; i--)
         {
             if (!inTime)
                 throw TimeOutException{};
             aux[nr[a[i]/p%n_16]]=a[i];
             nr[a[i]/p%n_16]--;
         }
-        for(int i=0; i<n; i++)
+        for(long long i=0; i<n; i++)
             a[i]=aux[i+1];
         p*=n_16;
     }
-    std::vector<int>().swap(nr);
-    std::vector<int>().swap(aux);
+    std::vector<long long>().swap(nr);
+    std::vector<long long>().swap(aux);
 }
 void ShellSort()
 {
-    vector<int> gaps;
-    int N = a.size();
+    vector<long long> gaps;
+    long long N = a.size();
     int l = int(log2(N));
     for(int i = 1; i <= l; i++)
-        gaps.push_back(2 * int(N / (pow(2, i) * 2)) + 1);
-    //for (int i = 0; i < gaps.size(); i++)
-        //cout<<gaps[i]<<" ";
-    //cout<<endl;
-    for(int x = 0; x < gaps.size(); x++)
+        gaps.push_back(2 * static_cast<long long>(N / (pow(2, i) * 2)) + 1);
+    for(long long x = 0; x < gaps.size(); x++)
     {
-        int gap = gaps[x];
-        for(int i = gap; i < N; i++)
+        long long gap = gaps[x];
+        for(long long i = gap; i < N; i++)
         {
             if (!inTime)
                 throw TimeOutException{};
-            int temp = a[i];
-            int j;
+            long long temp = a[i];
+            long long j;
             for(j = i; (j >= gap) && (a[j - gap] > temp); j -= gap)
                 a[j] = a[j - gap];
             a[j] = temp;
@@ -220,15 +215,22 @@ void ShellSort()
     }
 }
 
-int divide(int st,int dr)
+int divide(long long st,long long dr)
 {
-    int randomIndex = st + rand() % (dr - st + 1);
-    int piv = a[randomIndex];//pivot random
-    //int piv=a[dr];///alegem pivotul sa fie ultimul element
-    int i=st-1;
-    swap(a[randomIndex],a[dr]);
-
-    for(int j=st; j<dr; j++)
+    long long mij = st + (dr - st) / 2;
+    if (a[st] > a[mij]) {
+        swap(a[st], a[mij]);
+    }
+    if (a[mij] > a[dr]) {
+        swap(a[mij], a[dr]);
+    }
+    if (a[st] > a[mij]) {
+        swap(a[st], a[mij]);
+    }
+    long long piv = a[mij];
+    swap(a[mij], a[dr]);
+    long long i=st-1;
+    for(long long j=st; j<dr; j++)
         if(a[j]<piv)
         {
             i++;
@@ -237,19 +239,18 @@ int divide(int st,int dr)
     swap(a[i+1],a[dr]);
     return i+1;
 }
-void QuickSort(int st, int dr)
+void QuickSort(long long st, long long dr)
 {
     if (!inTime)
-            throw TimeOutException{};
+        throw TimeOutException{};
     if(st>=dr)
-    {
         return;
-    }
     int pivot=divide(st,dr);
     QuickSort(st,pivot-1);
     QuickSort(pivot+1,dr);
 }
 
+long long minVal, maxVal, elemCnt;
 bool isActive;
 double Time_sort(int tip)
 {
@@ -286,11 +287,15 @@ double Time_sort(int tip)
     }
     else if(tip==4)
     {
+        if (minVal < 0 || maxVal < 0)
+            throw 0;
         g<<"Timpul pentru RadixSortBaza10 este: ";
         RadixSort_10();
     }
     else if(tip==5)
     {
+        if (minVal < 0 || maxVal < 0)
+            throw 0;
         g<<"Timpul pentru RadixSortBaza2 este: ";
         RadixSort_16();
     }
@@ -304,6 +309,11 @@ double Time_sort(int tip)
         g<<"Timpul pentru QuickSort este: ";
         QuickSort(0 ,a.size()-1);
     }
+    else if(tip==8)
+    {
+        g<<"Timpul pentru IntroSort(default): ";
+        sort(a.begin(), a.end());
+    }
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     isActive = false;
@@ -316,8 +326,8 @@ void afisare()
 }
 bool is_sorted()
 {
-    int prev = a[0];
-    int i = 1;
+    long long prev = a[0];
+    long long i = 1;
     while (i < a.size() - 1)
     {
         if (a[i] < prev)
@@ -325,30 +335,35 @@ bool is_sorted()
         else prev = a[i];
         i++;
     }
-    std::vector<int>().swap(a);
+    std::vector<long long>().swap(a);
     return true;
 }
 
 int main()
 {
-    cout<<"Sortari: \nMerge-1\nInsertion-2\nHeap-3\nRadixB10-4\nRadixB2-5\nShell-6\nQuick-7\n";
+    cout<<"Sortari: \nMerge-1\nInsertion-2\nHeap-3\nRadixB10-4\nRadixB2-5\nShell-6\nQuick-7\nIntroSort-8\n";
     int S;
     cin>>S;
+    bool aproapeSortat;
+    cout<<"Aproape sortat?: ";
+    cin>>aproapeSortat;
+    cout<<endl;
     ifstream f("input.in");
-    int inputs, minVal, maxVal;
-    long long elemCnt;
+    int inputs;
     f>>inputs;
     for(int i=0; i<inputs; i++)
     {
         try{
         f>>minVal>>maxVal>>elemCnt;
         g<<minVal<<" "<<maxVal<<" "<<elemCnt<<" -> ";
-        cpy=get_number(minVal, maxVal, elemCnt);
+        if (aproapeSortat)
+            cpy = test_insertion(minVal, maxVal, elemCnt);
+        else
+           cpy=get_number(minVal, maxVal, elemCnt);
         g<<Time_sort(S)<<endl;
         g<<"Este bine sortat: "<<is_sorted()<<endl;
         }
-        catch(...){g<<"Nu poate sorta!\n";}
+        catch(...){g<<"Nu poate sorta!\n";std::vector<long long>().swap(a);}
     }
-    //afisare();
     return 0;
 }
